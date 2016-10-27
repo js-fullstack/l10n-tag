50137ba4ab40078c6b1c9ee1cf15b9a1af8383c9
shenyu1997 <shenyu1997@gmail.com>
2016-10-27 23:55:18 +0800

    US, add prefix and surffix support

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const debug = require('debug')('l10n-tag');
+const escape = require("escape-regexp");
 const REGEXP = /\$\{(\d+)\}/g;
+const PREFIX = '${';
+const SURFFIX = '}';
 
 function missingkey(key, inputs, values) {
     debug(`cannot found key: ${key} from l10n resource map.`);
 }
 
 module.exports = (l10nMap = {},options = {}) => {
-    let regexp = options.regexp || REGEXP;
     let cb = options.cb || missingkey;
+    let regexp = new RegExp(`${escape(options.prefix || PREFIX)}(\\d+)${escape(options.surffix || SURFFIX)}`, 'g');
+    debug('capture regexp',regexp.source);
     return (inputs, ...values) => {
         const key = inputs.slice(1).reduce((pre, curr, index) => `${pre}\${${index}}${curr}`, inputs[0]);
         if(!l10nMap[key]) {
