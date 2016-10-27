6ae89aea43b714998908cd7b77907c3820974190
Shen yu <yu.shen@hpe.com>
2016-10-27 05:34:55 +0000

    US, add optioins

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const debug = require('debug')('l10n-tag');
 const REGEXP = /\$\{(\d+)\}/g;
 
-module.exports = (l10nMap, output=debug) => {
+function missingkey(key, inputs, values) {
+    debug(`cannot found key: ${key} from l10n resource map.`);
+}
+
+module.exports = (l10nMap = {},options = {}) => {
+    let regexp = options.regexp || REGEXP;
+    let cb = options.cb || missingkey;
     return (inputs, ...values) => {
         const key = inputs.slice(1).reduce((pre, curr, index) => `${pre}\${${index}}${curr}`, inputs[0]);
         if(!l10nMap[key]) {
-            output(`cannot found key: ${key} from l10n resource map.`);
+            cb(key, inputs, values);
         }
         const template = l10nMap[key] || key;
-        return template.replace(REGEXP, (_,index) => values[index]);
+        return template.replace(regexp, (_,index) => values[index]);
     }
 }
