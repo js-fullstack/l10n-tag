87578be1923fa9ae7f8783b31094fb5643d404da
shenyu1997 <shenyu1997@gmail.com>
2016-10-27 11:52:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const debug = require('debug')('l10n-tag');
+const REGEXP = /\$\{(\d+)\}/g;
 
-module.exports = (l10nMap) => {
+module.exports = (l10nMap, output=debug) => {
     return (inputs, ...values) => {
         const key = inputs.slice(1).reduce((pre, curr, index) => `${pre}\${${index}}${curr}`, inputs[0]);
         if(!l10nMap[key]) {
-            debug(`cannot found key: ${key} from l10n resource map`);
+            output(`cannot found key: ${key} from l10n resource map.`);
         }
         const template = l10nMap[key] || key;
-        return template.replace(/\$\{(\d+)\}/g, (_,index) => values[index]);
+        return template.replace(REGEXP, (_,index) => values[index]);
     }
 }
